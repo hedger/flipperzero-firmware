253a3af543e5c840b993e5556042f48c8f7bc64b
hedger <hedger@nanode.su>
2021-11-28 10:45:20 +0300

    Moved chip data to spi_chips

--- applications/flash_manager/spi/spi_api.cpp
+++ applications/flash_manager/spi/spi_api.cpp
@@ -1,9 +1,10 @@
 #include "spi_api.h"
+#include "spi_chips.h"
 
 #include <furi.h>
 #include <furi-hal.h>
 
-#include "furi-hal-gpio.h"
+#include <furi-hal-gpio.h>
 
 #define TAG "SPIAPI"
 
@@ -170,171 +171,6 @@ void SpiToolkit::disconnect() {
     hal_gpio_init(&SPI_MISO, GpioModeAnalog, GpioPullNo, GpioSpeedVeryHigh);
 }
 
-#define SPI_CMD_JEDEC_ID 0x9F
-
-#define VENDOR_ID_CYPRESS 0x01
-#define VENDOR_ID_FUJITSU 0x04
-#define VENDOR_ID_EON 0x1C
-#define VENDOR_ID_ATMEL 0x1F
-#define VENDOR_ID_MICRON 0x20
-#define VENDOR_ID_AMIC 0x37
-#define VENDOR_ID_NOR_MEM 0x52
-#define VENDOR_ID_SANYO 0x62
-#define VENDOR_ID_INTEL 0x89
-#define VENDOR_ID_ESMT 0x8C
-#define VENDOR_ID_FUDAN 0xA1
-#define VENDOR_ID_HYUNDAI 0xAD
-#define VENDOR_ID_SST 0xBF
-#define VENDOR_ID_MICRONIX 0xC2
-#define VENDOR_ID_GIGADEVICE 0xC8
-#define VENDOR_ID_ISSI 0xD5
-#define VENDOR_ID_WINBOND 0xEF
-
-typedef struct {
-    const char* name;
-    uint8_t id;
-} VendorName_t;
-
-static const VendorName_t VendorNames[] = {
-    {"Cypress", VENDOR_ID_CYPRESS},
-    {"Fujitsu", VENDOR_ID_FUJITSU},
-    {"EON", VENDOR_ID_EON},
-    {"Atmel", VENDOR_ID_ATMEL},
-    {"Micron", VENDOR_ID_MICRON},
-    {"AMIC", VENDOR_ID_AMIC},
-    {"Sanyo", VENDOR_ID_SANYO},
-    {"Intel", VENDOR_ID_INTEL},
-    {"ESMT", VENDOR_ID_ESMT},
-    {"Fudan", VENDOR_ID_FUDAN},
-    {"Hyundai", VENDOR_ID_HYUNDAI},
-    {"SST", VENDOR_ID_SST},
-    {"GigaDevice", VENDOR_ID_GIGADEVICE},
-    {"ISSI", VENDOR_ID_ISSI},
-    {"Winbond", VENDOR_ID_WINBOND},
-    {"Micronix", VENDOR_ID_MICRONIX},
-    {"Nor-Mem", VENDOR_ID_NOR_MEM},
-    {NULL, 0}};
-
-typedef enum {
-    CHIP_WM_PAGE_256B = 0x01, /**< write 1 to 256 bytes per page */
-    CHIP_WM_BYTE = 0x02, /**< byte write */
-    CHIP_WM_AAI = 0x04, /**< auto address increment */
-    CHIP_WM_DUAL_BUFFER = 0x08, /**< dual-buffer write, like AT45DB series */
-} SpiWriteMode_t;
-
-typedef struct {
-    const char* name; /**< flash chip name */
-    uint8_t vendor_id; /**< vendor ID */
-    uint8_t type_id; /**< memory type ID */
-    uint8_t capacity_id; /**< capacity ID */
-    uint32_t size; /**< flash capacity (bytes) */
-    SpiWriteMode_t write_mode; /**< write mode */
-    uint32_t erase_gran; /**< erase granularity (bytes) */
-    uint8_t erase_gran_cmd; /**< erase granularity size block command */
-} ChipInfo_t;
-
-static const ChipInfo_t ChipInfos[] = {
-    {"AT45DB161E",
-     VENDOR_ID_ATMEL,
-     0x26,
-     0x00,
-     2L * 1024L * 1024L,
-     (SpiWriteMode_t)(CHIP_WM_BYTE | CHIP_WM_DUAL_BUFFER),
-     512,
-     0x81},
-    {"W25Q40BV", VENDOR_ID_WINBOND, 0x40, 0x13, 512L * 1024L, CHIP_WM_PAGE_256B, 4096, 0x20},
-    {"W25Q16BV", VENDOR_ID_WINBOND, 0x40, 0x15, 2L * 1024L * 1024L, CHIP_WM_PAGE_256B, 4096, 0x20},
-    {"W25Q32BV", VENDOR_ID_WINBOND, 0x40, 0x16, 4L * 1024L * 1024L, CHIP_WM_PAGE_256B, 4096, 0x20},
-    {"W25Q64CV", VENDOR_ID_WINBOND, 0x40, 0x17, 8L * 1024L * 1024L, CHIP_WM_PAGE_256B, 4096, 0x20},
-    {"W25Q64DW", VENDOR_ID_WINBOND, 0x60, 0x17, 8L * 1024L * 1024L, CHIP_WM_PAGE_256B, 4096, 0x20},
-    {"W25Q128BV", VENDOR_ID_WINBOND, 0x40, 0x18, 16L * 1024L * 1024L, CHIP_WM_PAGE_256B, 4096, 0x20},
-    {"W25Q256FV", VENDOR_ID_WINBOND, 0x40, 0x19, 32L * 1024L * 1024L, CHIP_WM_PAGE_256B, 4096, 0x20},
-    {"SST25VF080B",
-     VENDOR_ID_SST,
-     0x25,
-     0x8E,
-     1L * 1024L * 1024L,
-     (SpiWriteMode_t)(CHIP_WM_BYTE | CHIP_WM_AAI),
-     4096,
-     0x20},
-    {"SST25VF016B",
-     VENDOR_ID_SST,
-     0x25,
-     0x41,
-     2L * 1024L * 1024L,
-     (SpiWriteMode_t)(CHIP_WM_BYTE | CHIP_WM_AAI),
-     4096,
-     0x20},
-    {"M25P32",
-     VENDOR_ID_MICRON,
-     0x20,
-     0x16,
-     4L * 1024L * 1024L,
-     CHIP_WM_PAGE_256B,
-     64L * 1024L,
-     0xD8},
-    {"M25P80",
-     VENDOR_ID_MICRON,
-     0x20,
-     0x14,
-     1L * 1024L * 1024L,
-     CHIP_WM_PAGE_256B,
-     64L * 1024L,
-     0xD8},
-    {"M25P40", VENDOR_ID_MICRON, 0x20, 0x13, 512L * 1024L, CHIP_WM_PAGE_256B, 64L * 1024L, 0xD8},
-    {"EN25Q32B", VENDOR_ID_EON, 0x30, 0x16, 4L * 1024L * 1024L, CHIP_WM_PAGE_256B, 4096, 0x20},
-    {"GD25Q64B",
-     VENDOR_ID_GIGADEVICE,
-     0x40,
-     0x17,
-     8L * 1024L * 1024L,
-     CHIP_WM_PAGE_256B,
-     4096,
-     0x20},
-    {"GD25Q16B",
-     VENDOR_ID_GIGADEVICE,
-     0x40,
-     0x15,
-     2L * 1024L * 1024L,
-     CHIP_WM_PAGE_256B,
-     4096,
-     0x20},
-    {"GD25Q32C",
-     VENDOR_ID_GIGADEVICE,
-     0x40,
-     0x16,
-     4L * 1024L * 1024L,
-     CHIP_WM_PAGE_256B,
-     4096,
-     0x20},
-    {"S25FL216K", VENDOR_ID_CYPRESS, 0x40, 0x15, 2L * 1024L * 1024L, CHIP_WM_PAGE_256B, 4096, 0x20},
-    {"S25FL032P", VENDOR_ID_CYPRESS, 0x02, 0x15, 4L * 1024L * 1024L, CHIP_WM_PAGE_256B, 4096, 0x20},
-    {"A25L080", VENDOR_ID_AMIC, 0x30, 0x14, 1L * 1024L * 1024L, CHIP_WM_PAGE_256B, 4096, 0x20},
-    {"F25L004",
-     VENDOR_ID_ESMT,
-     0x20,
-     0x13,
-     512L * 1024L,
-     (SpiWriteMode_t)(CHIP_WM_BYTE | CHIP_WM_AAI),
-     4096,
-     0x20},
-    {"PCT25VF016B",
-     VENDOR_ID_SST,
-     0x25,
-     0x41,
-     2L * 1024L * 1024L,
-     (SpiWriteMode_t)(CHIP_WM_BYTE | CHIP_WM_AAI),
-     4096,
-     0x20},
-    {"NM25Q128EV",
-     VENDOR_ID_NOR_MEM,
-     0x21,
-     0x18,
-     16L * 1024L * 1024L,
-     CHIP_WM_PAGE_256B,
-     4096,
-     0x20},
-    {NULL, 0, 0, 0, 0}};
 
 static bool read_sfdp() {
     return false;
@@ -378,7 +214,7 @@ bool SpiToolkit::detect_flash() {
         hal_gpio_init(&SPI_MISO, GpioModeInput, GpioPullDown, GpioSpeedVeryHigh);
 
         uint8_t id[3];
-        if(write_read(SPI_CMD_JEDEC_ID, NULL, 0, id, 3)) {
+        if(write_read(SpiChipCommand_JEDEC_ID, NULL, 0, id, 3)) {
             last_info.vendor_id = id[0];
             last_info.type_id = id[1];
             last_info.capacity_id = id[2];
@@ -417,7 +253,6 @@ bool SpiToolkit::chip_erase() {
 
     osDelay(4000);
     // TODO: chip_erase
-
     return true;
 }
 
@@ -433,6 +268,7 @@ bool SpiToolkit::write_block(
     const size_t offset,
     const uint8_t* const p_data,
     const size_t data_len) {
+
     furi_assert(p_data);
     furi_assert(data_len && (data_len <= SPI_MAX_BLOCK_SIZE));
 
@@ -451,7 +287,7 @@ bool SpiToolkit::read_block(const size_t offset, uint8_t* const p_data, const si
 
     osDelay(10);
     // TODO: read_block
-    FURI_LOG_I(TAG, "memset @ %x len %x", p_data, data_len);
+    //FURI_LOG_I(TAG, "memset @ %x len %x", p_data, data_len);
     memset(p_data, 0xCD, data_len);
 
     return true;
---
+++ applications/flash_manager/spi/spi_chips.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef enum {
+    SpiChipCommand_JEDEC_ID = 0x9f,
+} SpiChipCommand;
+
+typedef enum {
+    SpiChipVendor_CYPRESS = 0x01,
+    SpiChipVendor_FUJITSU = 0x04,
+    SpiChipVendor_EON = 0x1C,
+    SpiChipVendor_ATMEL = 0x1F,
+    SpiChipVendor_MICRON = 0x20,
+    SpiChipVendor_AMIC = 0x37,
+    SpiChipVendor_NOR_MEM = 0x52,
+    SpiChipVendor_SANYO = 0x62,
+    SpiChipVendor_INTEL = 0x89,
+    SpiChipVendor_ESMT = 0x8C,
+    SpiChipVendor_FUDAN = 0xA1,
+    SpiChipVendor_HYUNDAI = 0xAD,
+    SpiChipVendor_SST = 0xBF,
+    SpiChipVendor_MICRONIX = 0xC2,
+    SpiChipVendor_GIGADEVICE = 0xC8,
+    SpiChipVendor_ISSI = 0xD5,
+    SpiChipVendor_WINBOND = 0xEF,
+} SpiChipVendor;
+
+typedef struct {
+    const char* name;
+    const uint8_t id;
+} VendorName_t;
+
+typedef enum {
+    CHIP_WM_PAGE_256B = 0x01, /**< write 1 to 256 bytes per page */
+    CHIP_WM_BYTE = 0x02, /**< byte write */
+    CHIP_WM_AAI = 0x04, /**< auto address increment */
+    CHIP_WM_DUAL_BUFFER = 0x08, /**< dual-buffer write, like AT45DB series */
+} SpiWriteMode_t;
+
+typedef struct {
+    const char* name; /**< flash chip name */
+    uint8_t vendor_id; /**< vendor ID */
+    uint8_t type_id; /**< memory type ID */
+    uint8_t capacity_id; /**< capacity ID */
+    uint32_t size; /**< flash capacity (bytes) */
+    SpiWriteMode_t write_mode; /**< write mode */
+    uint32_t erase_gran; /**< erase granularity (bytes) */
+    uint8_t erase_gran_cmd; /**< erase granularity size block command */
+} ChipInfo_t;
+
+
+extern const VendorName_t VendorNames[];
+extern const ChipInfo_t ChipInfos[];
